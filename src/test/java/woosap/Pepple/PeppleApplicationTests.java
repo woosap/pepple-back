fb06bbf7eb6e7b6ad78a8b086a0228aa8db7e77a
Jaycbot <jay0764@naver.com>
2021-06-28 17:30:48 +0900

    초기 환경설정

---
+++ src/test/java/woosap/Pepple/PeppleApplicationTests.java
@@ -0,0 +1,13 @@
+package woosap.Pepple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeppleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
