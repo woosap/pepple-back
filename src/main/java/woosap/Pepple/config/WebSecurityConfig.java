0747a1d59c003404146b7324f2cca971dd420dd0
jay-choe <jay0764@naver.com>
2021-07-08 16:50:58 +0900

    Add OAuth2 security config

--- src/main/java/woosap/Pepple/config/WebSecurityConfig.java
+++ src/main/java/woosap/Pepple/config/WebSecurityConfig.java
@@ -1,7 +1,52 @@
 package woosap.Pepple.config;
 
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+            .antMatchers("/",
+            "/error",
+            "/favicon.ico",
+            "/**/*.png",
+            "/**/*.gif",
+            "/**/*.svg",
+            "/**/*.jpg",
+            "/**/*.html",
+            "/**/*.css",
+            "/**/*.js");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic()
+                .disable()
+            .formLogin()
+                .disable()
+            .cors()
+            .and()
+            .authorizeRequests()
+                .antMatchers("/auth/**", "/oauth2/**")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+                    .and()
+            .oauth2Login()
+                .authorizationEndpoint()
+                    .baseUri("/oauth2/authorize")
+                    .and()
+                .redirectionEndpoint()
+                    .baseUri("/oauth2/callback/*");
+
+    }
 }
