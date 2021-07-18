bbb2fdef84640291ea00ca1bc0ca111b38db6aee
jay-choe <jay0764@naver.com>
2021-07-18 19:33:26 +0900

    Add validation

--- src/main/java/woosap/Pepple/dto/UserDTO.java
+++ src/main/java/woosap/Pepple/dto/UserDTO.java
@@ -3,6 +3,7 @@ package woosap.Pepple.dto;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,8 +16,10 @@ import woosap.Pepple.entity.type.Job;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO implements Serializable {
+    @NotBlank(message = "아이디를 반드시 입력해야 합니다")
     private String userId;
 
+    @NotBlank(message = "닉네임은 필수 항목입니다")
     private String nickname;
 
     private String imageUrl;
