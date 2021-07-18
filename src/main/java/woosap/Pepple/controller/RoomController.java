f1e22ac126e2ae51058686ac6ec72ce6c804d2db
wonchoi <my970625@gmail.com>
2021-07-19 01:12:10 +0900

    Feat/#13 RoomController 수정중입니다

--- src/main/java/woosap/Pepple/controller/RoomController.java
+++ src/main/java/woosap/Pepple/controller/RoomController.java
@@ -1,10 +1,18 @@
 package woosap.Pepple.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import woosap.Pepple.dto.ResponseDTO;
+import woosap.Pepple.dto.RoomDTO;
 import woosap.Pepple.security.TokenServiceImpl;
+import woosap.Pepple.service.RoomServiceImpl;
 import woosap.Pepple.service.UserServiceImpl;
 
 @RestController
@@ -15,6 +23,14 @@ public class RoomController {
 
     private final UserServiceImpl userService;
     private final TokenServiceImpl tokenService;
+    private final RoomServiceImpl roomService;
 
+    @GetMapping("/create")
+    public ResponseEntity<?> creatRoom(@Valid RoomDTO roomInfo,
+        HttpServletRequest httpServletRequest) {
+
+        roomService.createRoom(roomInfo);
+        return new ResponseEntity<>(new ResponseDTO("방을 만들었습니다", true), HttpStatus.CREATED);
+    }
 
 }
