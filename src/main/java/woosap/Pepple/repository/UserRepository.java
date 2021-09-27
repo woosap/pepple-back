9dc171175c2cf222dcd4981cc5f98fdb059e448e
jay-choe <jay0764@naver.com>
2021-09-28 02:51:49 +0900

    쿼리 에러 수정

--- src/main/java/woosap/Pepple/repository/UserRepository.java
+++ src/main/java/woosap/Pepple/repository/UserRepository.java
@@ -1,13 +1,8 @@
 package woosap.Pepple.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import woosap.Pepple.entity.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
     Boolean existsByNickname(String nickname);
-
-    @Query("SELECT u FROM User u join fetch u.snsList")
-    Optional<User> findById(String userId);
 }
