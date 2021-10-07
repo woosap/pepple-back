d9c2564e8d88502a14bb78c80d9c363f6a793611
jay-choe <jay0764@naver.com>
2021-10-07 21:45:34 +0900

    프론트엔드 배포로 인한 Redirect URL 변경

--- src/main/java/woosap/Pepple/util/Constants.java
+++ src/main/java/woosap/Pepple/util/Constants.java
@@ -5,5 +5,5 @@ public abstract class Constants {
     public static String GOOGLE = "google";
     public static String GITHUB = "github";
     public static String SESSION_KEY = "userId";
-    public static String REDIRECT_URL = "https://woosap.github.io/pepple-front/redirect";
+    public static String REDIRECT_URL = "https://pepple.link/redirect";
 }
